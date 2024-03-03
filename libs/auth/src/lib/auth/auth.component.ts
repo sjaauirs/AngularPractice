29c0c71947cb6391b4537d26a56b47a45398e81b
saurabh <saurabh28jaiswal@gmail.com>
2024-03-04 00:41:43 +0530

    Ading a Library for Auth Library is intended to use as a module Library will build with Application code
    Executing task: npx nx generate @nx/angular:library --name=auth --directory=libs/auth --routing=true --projectNameAndRootFormat=as-provided --no-interactive

---
+++ libs/auth/src/lib/auth/auth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'my-nx-angular-workspace-auth',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css',
+})
+export class AuthComponent {}
